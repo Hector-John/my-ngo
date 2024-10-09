cb31cce886b535a421f1b566bc0cd4606d397779
Hector-John <hectorjohn254@gmail.com>
2024-10-09 19:44:07 +0300

    final

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,54 +1,50 @@
-import banner from '../assets/banner.png'
-import bgimg from '../assets/bgimg.png'
-import cert from '../assets/cert.png'
-import cleanWater from '../assets/cleanWater.png'
-import ctaBg from '../assets/ctaBg.png'
-import demo from '../assets/demo.png'
-import footer from '../assets/footer.png'
-import gallery1 from '../assets/gallery1.png'
-import gallery2 from '../assets/gallery2.png'
-import gallery3 from '../assets/gallery3.png'
-import gallery4 from '../assets/gallery4.png'
-import gallery5 from '../assets/gallery5.png'
-import givingPrize from '../assets/givingPrize.png'
-import handshake from '../assets/handshake.png'
-import image1 from '../assets/image1.png'
-import logo from '../assets/logo.png'
-import logoh from '../assets/logoh.png'
-import office from '../assets/office.png'
-import parade from '../assets/parade.png'
-import separator from '../assets/separator.png'
-import slider1 from '../assets/slider1.png'
-import slider2 from '../assets/slider2.png'
-import slider3 from '../assets/slider3.png'
-import slider4 from '../assets/slider4.png'
-import mercury from '../assets/mercury.png'
-import microlend from '../assets/microlend.png'
-import sponsor1 from '../assets/sponsor1.png'
-import sponsor2 from '../assets/sponsor2.png'
-import sponsor6 from '../assets/sponsor6.png'
-import teaching from '../assets/teaching.png'
-import teachingCommunity from '../assets/teachingCommunity.png'
-import team from '../assets/team.png'
-import teenGirls from '../assets/teenGirls.png'
-import volunteer1 from '../assets/volunteer1.png'
-import youngGirls from '../assets/youngGirls.png'
-import sponsorsvg1 from '../assets/sponsorsvg1.svg';
-import sponsorsvg2 from '../assets/sponsorsvg2.svg'; 
-import sponsorsvg3 from '../assets/sponsorsvg3.svg';
-import sponsorsvg4 from '../assets/sponsorsvg4.svg';
-import sponsorsvg5 from '../assets/sponsorsvg5.svg';
-import avatar1 from '../assets/avatar1.png';
-import avatar2 from '../assets/avatar2.png';
-import avatar3 from '../assets/avatar3.png';
-import avatar4 from '../assets/avatar4.png';
-import volunteers from '../assets/volunteers.png';
-import testimonybg from '../assets/testimonybg.png';
-import causesBg from '../assets/causesBg.png';
-
-
-
-
+import banner from "../assets/banner.png";
+import bgimg from "../assets/bgimg.png";
+import cert from "../assets/cert.png";
+import cleanWater from "../assets/cleanWater.png";
+import ctaBg from "../assets/ctaBg.png";
+import demo from "../assets/demo.png";
+import footer from "../assets/footer.png";
+import gallery1 from "../assets/gallery1.png";
+import gallery2 from "../assets/gallery2.png";
+import gallery3 from "../assets/gallery3.png";
+import gallery4 from "../assets/gallery4.png";
+import gallery5 from "../assets/gallery5.png";
+import givingPrize from "../assets/givingPrize.png";
+import handshake from "../assets/handshake.png";
+import image1 from "../assets/image1.png";
+import logo from "../assets/logo.png";
+import logoh from "../assets/logoh.png";
+import office from "../assets/office.png";
+import parade from "../assets/parade.png";
+import separator from "../assets/separator.png";
+import slider1 from "../assets/slider1.png";
+import slider2 from "../assets/slider2.png";
+import slider3 from "../assets/slider3.png";
+import slider4 from "../assets/slider4.png";
+import mercury from "../assets/mercury.png";
+import microlend from "../assets/microlend.png";
+import sponsor1 from "../assets/sponsor1.png";
+import sponsor2 from "../assets/sponsor2.png";
+import sponsor6 from "../assets/sponsor6.png";
+import teaching from "../assets/teaching.png";
+import teachingCommunity from "../assets/teachingCommunity.png";
+import team from "../assets/team.png";
+import teenGirls from "../assets/teenGirls.png";
+import volunteer1 from "../assets/volunteer1.png";
+import youngGirls from "../assets/youngGirls.png";
+import sponsorsvg1 from "../assets/sponsorsvg1.svg";
+import sponsorsvg2 from "../assets/sponsorsvg2.svg";
+import sponsorsvg3 from "../assets/sponsorsvg3.svg";
+import sponsorsvg4 from "../assets/sponsorsvg4.svg";
+import sponsorsvg5 from "../assets/sponsorsvg5.svg";
+import avatar1 from "../assets/avatar1.png";
+import avatar2 from "../assets/avatar2.png";
+import avatar3 from "../assets/avatar3.png";
+import avatar4 from "../assets/avatar4.png";
+import volunteers from "../assets/volunteers.png";
+import testimonybg from "../assets/testimonybg.png";
+import causesBg from "../assets/causesBg.png";
 
 const images = {
   causesBg,
@@ -64,40 +60,40 @@ const images = {
   sponsorsvg4,
   sponsorsvg5,
   sponsor6,
-    banner,
-    bgimg,
-    cert,
-    cleanWater,
-    ctaBg,
-    demo,
-    footer,
-    gallery1,
-    gallery2,
-    gallery3,
-    gallery4,
-    gallery5,
-    givingPrize,
-    handshake,
-    image1,
-    logo,
-    logoh,
-    office,
-    parade,
-    separator,
-    slider1,
-    slider2,
-    slider3,
-    slider4,
-    mercury,
-    microlend,
-    sponsor1,
-    sponsor2,
-    teaching,
-    teachingCommunity,
-    team,
-    teenGirls,
-    volunteer1,
-    youngGirls,
-  };
-  
-  export default images;
+  banner,
+  bgimg,
+  cert,
+  cleanWater,
+  ctaBg,
+  demo,
+  footer,
+  gallery1,
+  gallery2,
+  gallery3,
+  gallery4,
+  gallery5,
+  givingPrize,
+  handshake,
+  image1,
+  logo,
+  logoh,
+  office,
+  parade,
+  separator,
+  slider1,
+  slider2,
+  slider3,
+  slider4,
+  mercury,
+  microlend,
+  sponsor1,
+  sponsor2,
+  teaching,
+  teachingCommunity,
+  team,
+  teenGirls,
+  volunteer1,
+  youngGirls,
+};
+
+export default images;
